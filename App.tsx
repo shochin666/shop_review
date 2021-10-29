ea4ef9ef7e82ac96cbd28ead0850df67f63975cf
shochin666 <shoogawa111@gmail.com>
2021-10-29 17:46:29 +0900

    show shops info

--- App.tsx
+++ App.tsx
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { getShops } from './src/lib/firebase'
+import { Shop } from './src/lib/types/shop';
+import { ShopReviewItems } from './src/components/ShopReviewItems';
 
 export default function App() {
+
+  const [shops, setShops] = useState<Shop[]>([])
+
+  useEffect(() => {
+    getFirebaseItems();
+  }, []);
+
+  const getFirebaseItems = async() => {
+    const shops = await getShops();
+    setShops(shops);
+  }
+
+  const shopItems = shops.map((shop, index) => {
+    return(
+      <View>
+        <ShopReviewItems shop={shop} key={index.toString()} />
+      </View>
+    )
+  })
+
   return (
     <View style={styles.container}>
-      <Text>Hello!</Text>
+      {shopItems}
       <StatusBar style="auto" />
     </View>
   );
